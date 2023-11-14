c4245d188c16ca8df71b6f64f4d8f6bace2de7c3
Ohtashu <89231661+Ohtashu@users.noreply.github.com>
2023-11-14 12:02:13 +0800

    Add files via upload

---
+++ OOP-ASSIGN/OOP-ASSIGN/Cat.java
@@ -0,0 +1,14 @@
+class Cat extends Animal {
+    public void eat()
+       {
+        System.out.println("Cat Food");
+       }
+       public void sleep()
+       {
+        System.out.println("And Sleep around 15 to 16 Hours");
+       }
+       public void makeSound()
+       {
+        System.out.println("Meow Meow. ");
+       }
+}
